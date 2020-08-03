d2e14207428534506276cce51b3a22196f6fd8e0
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-03 11:29:05 -0500

    added pseudocode for daily schedules purpose.

--- Develop/script.js
+++ Develop/script.js
@@ -4,3 +4,10 @@ $("#currentDay").text(moment().format("dddd MMMM Do"));
 $(".time-block").each(function() {
     console.log($(this).children())
 })
+
+// Need to color code time blocks based off time of day based off assigned classes.
+// Present hour is highlighted in red.
+// Future hours are highlighted in green.
+// Past hours are gray. 
+// Want to add the days schedule to the respective time slots and save it.
+// If the user refreshes or exits out of the page and returns later, the daily items are saved. 
