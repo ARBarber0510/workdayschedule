8146be72a1fa829e97ac8dbd3053366d5dee34ca
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-03 12:35:08 -0500

    debugging issues with local storage.

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,74 @@
 $(document).ready(function() {
 
-$("#currentDay").text(moment().format("dddd MMMM Do"));
+    $("#currentDay").text(moment().format("dddd MMMM Do"));
 
-
-$(".time-block").each(function() {
-    console.log($(this).children())
-})
-
-// SaveBtn listener for user input assigned.
-$(".saveBtn").on("click", function() {
-    console.log(this);
-
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
-    localStorage.setItem(time, text);
-})
-
-$("#hour-9" .description).val(localStorage.getItem("hour-9"));
-$("#hour-10" .description).val(localStorage.getItem("hour-10"));
-$("#hour-11" .description).val(localStorage.getItem("hour-11"));
-$("#hour-12" .description).val(localStorage.getItem("hour-12"));
-$("#hour-13" .description).val(localStorage.getItem("hour-13"));
-$("#hour-14" .description).val(localStorage.getItem("hour-14"));
-$("#hour-15" .description).val(localStorage.getItem("hour-15"));
-$("#hour-16" .description).val(localStorage.getItem("hour-16"));
-$("#hour-17" .description).val(localStorage.getItem("hour-17"));
-
-
-function hourTracker() {
-    var presentHour = moment().hour();
 
     $(".time-block").each(function() {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockHour, presentHour)
-
-        if (blockHour < presentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
-        }
-        else if (blockHour === presentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else if (blockHour > presentHour) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
+        console.log($(this).children())
     })
-}
 
-hourTracker();
+
+    var $text9AM = $("#hour-9");
+    var $text10AM = $("#hour-10");
+    var $text11AM = $("#hour-11");
+    var $text12PM = $("#hour-12");
+    var $text1PM = $("#hour-13");
+    var $text2PM = $("#hour-14");
+    var $text3PM = $("#hour-15");
+    var $text4PM = $("#hour-16");
+    var $text5PM = $("#hour-17");
+
+    // SaveBtn listener for user input assigned.
+    $(".saveBtn").on("click", function() {
+        console.log(this);
+
+        localStorage.setItem("9AM", ($text9AM.val()))
+        localStorage.setItem("10AM", ($text10AM.val()))
+        localStorage.setItem("11AM", ($text11AM.val()))
+        localStorage.setItem("12PM", ($text12PM.val()))
+        localStorage.setItem("1PM", ($text1PM.val()))
+        localStorage.setItem("2PM", ($text2PM.val()))
+        localStorage.setItem("3PM", ($text3PM.val()))
+        localStorage.setItem("4PM", ($text4PM.val()))
+        localStorage.setItem("5PM", ($text5PM.val()))
+
+        $("#text9AM").append(localStorage.getItem("9AM"));
+        $("#text10AM").append(localStorage.getItem("10AM"));
+        $("#text11AM").append(localStorage.getItem("11AM"));
+        $("#text12PM").append(localStorage.getItem("12PM"));
+        $("#text1PM").append(localStorage.getItem("1PM"));
+        $("#text2PM").append(localStorage.getItem("2PM"));
+        $("#text3PM").append(localStorage.getItem("3PM"));
+        $("#text4PM").append(localStorage.getItem("4PM"));
+        $("#text5PM").append(localStorage.getItem("5PM"));
+    })
+
+    function hourTracker() {
+        var presentHour = moment().hour();
+
+        $(".time-block").each(function() {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, presentHour)
+
+            if (blockHour < presentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockHour === presentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockHour > presentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    hourTracker();
 })
 
 // Need to color code time blocks based off time of day based off assigned classes.
