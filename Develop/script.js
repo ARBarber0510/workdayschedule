cc24bd3d6c15922ca6d32ea8677aaf78a34c3e7c
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-03 11:53:36 -0500

    added local storage for each hour.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 $("#currentDay").text(moment().format("dddd MMMM Do"));
 
 
@@ -5,9 +7,26 @@ $(".time-block").each(function() {
     console.log($(this).children())
 })
 
+// SaveBtn listener for user input assigned.
 $(".saveBtn").on("click", function() {
     console.log(this);
+
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
 })
+
+$("#hour-9" .description).val(localStorage.getItem("hour-9"));
+$("#hour-10" .description).val(localStorage.getItem("hour-10"));
+$("#hour-11" .description).val(localStorage.getItem("hour-11"));
+$("#hour-12" .description).val(localStorage.getItem("hour-12"));
+$("#hour-13" .description).val(localStorage.getItem("hour-13"));
+$("#hour-14" .description).val(localStorage.getItem("hour-14"));
+$("#hour-15" .description).val(localStorage.getItem("hour-15"));
+$("#hour-16" .description).val(localStorage.getItem("hour-16"));
+$("#hour-17" .description).val(localStorage.getItem("hour-17"));
+
 
 function hourTracker() {
     var presentHour = moment().hour();
@@ -34,6 +53,8 @@ function hourTracker() {
     })
 }
 
+hourTracker();
+})
 
 // Need to color code time blocks based off time of day based off assigned classes.
 // Present hour is highlighted in red.
