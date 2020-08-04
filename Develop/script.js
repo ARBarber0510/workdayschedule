45ce6e688a0f9cd53261d500de9ab6be6733509f
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-04 11:13:18 -0500

    added comments to js.

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-
+    // Display current date.
     $("#currentDay").text(moment().format("dddd MMMM Do"));
 
-    // Storing
+    // Assign saveBtn click listener for user input. 
     $(".saveBtn").on("click", function() {
         console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).siblings("textarea").attr("id");
 
+        // Set items in localStorage.
         localStorage.setItem(time, text);
     })
     
+    // Load saved data from localStorage for individual hour on schedule.
     $("#hour-9").val(localStorage.getItem("hour-9"));
     $("#hour-10").val(localStorage.getItem("hour-10"));
     $("#hour-11").val(localStorage.getItem("hour-11"));
@@ -21,16 +23,17 @@ $(document).ready(function() {
     $("#hour-16").val(localStorage.getItem("hour-16"));
     $("#hour-17").val(localStorage.getItem("hour-17"));
 
-    // Trying to target each hour ID and addClass past, present, future based on time of day.
+    // Track each hour to compare to current time of day.
     function hourTracker() {
-        // Console log returning presentHour not defined in console.
+        // Target present hour.
         var presentHour = moment().hours();
 
+        // Loop through time blocks.
         $(".description").each(function() {
-            // console.log($(this).children())
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
             console.log(blockHour, presentHour)
-    
+            
+            // Past, present, future classes added to blockHour based off time of day from moment.js.
             if (blockHour < presentHour) {
                 $(this).addClass("past")
             }
@@ -44,5 +47,5 @@ $(document).ready(function() {
         })
     }
     hourTracker();
-    
+
 })
