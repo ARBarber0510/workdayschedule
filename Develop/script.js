6eb00659ac718adf33226387a2673e03666301e2
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-04 10:49:04 -0500

    remived pseudocode.

--- Develop/script.js
+++ Develop/script.js
@@ -45,12 +45,4 @@ $(document).ready(function() {
     }
     hourTracker();
     
-    
 })
-
-// Need to color code time blocks based off time of day based off assigned classes.
-// Present hour is highlighted in red.
-// Future hours are highlighted in green.
-// Past hours are gray. 
-// Want to add the days schedule to the respective time slots and save it.
-// If the user refreshes or exits out of the page and returns later, the daily items are saved. 
